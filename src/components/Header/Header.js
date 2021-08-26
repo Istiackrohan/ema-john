490ea3692c92f01d3019635a7082ddd531a5767a
Istiackrohan <istiackrohan@gmail.com>
2021-08-26 22:30:23 +0600

    header e image add korsi

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <img src="/ema-john-simple/src/images/logo.png" alt="" />
+        </div>
+    )
+}
+
+export default Header
